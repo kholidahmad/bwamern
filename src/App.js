85f19c4f315331116751e86a9c5f276c947f7db8
kholidahmad <ahmatcholid@gmail.com>
2020-11-16 13:57:50 +0700

    test button

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import React from 'react';
 import "assets/scss/style.scss";
 
 function App() {
-  return (
-    <div className="App">
-      Hallo Sam Kholid.
-    </div>
-  );
+  return <div className="App">Hallo Sam Kholid.</div>;
 }
 
 export default App;
