f09c086dae5416def631e0b57fc5610d3bc2d09a
kholidahmad <ahmatcholid@gmail.com>
2020-11-23 16:15:49 +0700

    create section hero

--- src/pages/LandingPages.js
+++ src/pages/LandingPages.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import Header from "parts/Header"
+import Header from "parts/Header";
+
+//ambil data dari landingPage.json
+import landingPage from "json/landingPage.json";
+import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
+
 export default class LandingPages extends Component {
   render() {
     return (
       <>
-       <Header {...this.props}></Header> 
+        <Header {...this.props}></Header> 
+        <Hero data={landingPage.hero}/>
+        <MostPicked data={landingPage.mostPicked}/>
       </>
     )
   }
