f0ff18ece01eeb2e2b622d2c9520499a680f6253
kholidahmad <ahmatcholid@gmail.com>
2020-11-25 19:54:02 +0700

    create component mostPicked & categories

--- src/pages/LandingPages.js
+++ src/pages/LandingPages.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 
 import Header from "parts/Header";
-
 //ambil data dari landingPage.json
-import landingPage from "json/landingPage.json";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
+import landingPage from "json/landingPage.json";
 
 export default class LandingPages extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
   render() {
     return (
       <>
         <Header {...this.props}></Header> 
-        <Hero data={landingPage.hero}/>
-        <MostPicked data={landingPage.mostPicked}/>
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero}/>
+        <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
+        <Categories data={landingPage.categories}/>
       </>
     )
   }
--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -12,7 +12,7 @@ export default function Hero(props) {
 
   function showMostPicked() {
     window.scrollTo({
-      top: props.refMostPicked.current.offsetTop = 30,
+      top: props.refMostPicked.current.offsetTop - 30,
       behavior: "smooth"
     });
   }
